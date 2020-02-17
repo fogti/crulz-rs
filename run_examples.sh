59b2538ca360f8d035b6ababe0debf4fdca7061b
zseri <zseri.devel@ytrizja.de>
2020-02-18 00:12:21 +0100

    make 'run_examples.sh' target-dir'-independent

--- run_examples.sh
+++ run_examples.sh
@@ -1,13 +1,24 @@
-#!/bin/bash
+#!/bin/sh
+
+if ! [ -x "$(which jq)" ]; then
+  echo "run_examples.sh: jq not found!"
+  exit 1
+fi
 
 cd "$(dirname "$0")"
 cargo build --release || exit $?
 echo
 
+CRULZ="$(cargo metadata --format-version 1 | jq -r '.target_directory')/release/crulz"
+if ! [ -x "$CRULZ" ]; then
+  echo "run_examples.sh: crulz not found!"
+  exit 1
+fi
+
 for i in examples/*; do
   echo "$i"
   cat "$i"
-  target/release/crulz "$@" "$i"
+  "$CRULZ" "$@" "$i"
   #time target/release/crulz "$@" "$i"
   echo
 done
