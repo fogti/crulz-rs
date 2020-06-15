d2a5d62801e5f1cbc8d2f81e179e71dd10cc4436
zseri <zseri.devel@ytrizja.de>
2020-06-15 19:32:18 +0200

    interned strings :+ predefined commands

--- build.rs
+++ build.rs
@@ -6,7 +6,24 @@ fn main() {
     println!("cargo:rerun-if-changed=build.rs");
 
     string_cache_codegen::AtomType::new("crulst::CrulzAtom", "crulst_atom!")
-        .atoms(&[" ", "$", "{", "}"])
+        .atoms(&[
+            " ",
+            "$",
+            "{",
+            "}",
+            "add",
+            "def",
+            "def-lazy",
+            "foreach",
+            "foreach-raw",
+            "fseq",
+            "include",
+            "pass",
+            "suppress",
+            "undef",
+            "une",
+            "unee",
+        ])
         .write_to_file(&Path::new(&env::var("OUT_DIR").unwrap()).join("crulst_atom.rs"))
         .unwrap()
 }
