e9f8fbb5ac8aeda15ea718c029c503b140762d1b
zseri <zseri.devel@ytrizja.de>
2021-02-19 14:30:38 +0100

    fix flake + overlay

--- flake.nix
+++ flake.nix
@@ -3,22 +3,25 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-20.09";
     flake-utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
   };
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem (flake-utils.lib.allSystems)
+    let
+      progname = "crulz";
+      overlay = final: prev: {
+        ${progname} = (prev.pkgs.callPackage ./Cargo.nix {}).rootCrate.build;
+      };
+    in {
+      overlay = overlay;
+    } // flake-utils.lib.eachDefaultSystem
       (system:
         rec {
-          packages.crulz = (nixpkgs.callPackage ./Cargo.nix {}).rootCrate.build;
-          defaultPackage = packages.crulz;
-          apps.crulz = flake-utils.lib.mkApp { drv = packages.crulz; };
-          defaultApp = apps.crulz;
-          overlay = selfx: superx: {
-            crulz = packages.crulz;
-          };
+          defaultPackage = (import nixpkgs {
+            inherit system;
+            overlays = [ overlay ];
+          }).${progname};
+          defaultApp = flake-utils.lib.mkApp { drv = defaultPackage; };
+          packages.${progname} = defaultPackage;
+          apps.${progname} = defaultApp;
         }
       );
 }
