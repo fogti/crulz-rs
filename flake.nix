d1cc1ab8005483d9a8ab29e689ab316d5131a6a7
zseri <zseri.devel@ytrizja.de>
2021-03-31 21:30:35 +0200

    Nix: use YZITE/flake-utils

--- flake.nix
+++ flake.nix
@@ -2,31 +2,17 @@
   description = "a rust implementation of the 'crulz' macro language interpreter";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-20.09";
-    flake-utils.url = "github:numtide/flake-utils";
+    yz-flake-utils.url = "github:YZITE/flake-utils";
     # needed for default.nix, shell.nix
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
-  outputs = { self, nixpkgs, flake-utils, flake-compat }:
-    let
+  outputs = { nixpkgs, yz-flake-utils, ... }:
+    yz-flake-utils.lib.mkFlakeFromProg {
+      prevpkgs = nixpkgs;
       progname = "crulz";
-      overlay = final: prev: {
-        ${progname} = (prev.pkgs.callPackage ./Cargo.nix {}).rootCrate.build;
-      };
-    in {
-      overlay = overlay;
-    } // flake-utils.lib.eachDefaultSystem
-      (system:
-        rec {
-          defaultPackage = (import nixpkgs {
-            inherit system;
-            overlays = [ overlay ];
-          }).${progname};
-          defaultApp = flake-utils.lib.mkApp { drv = defaultPackage; };
-          packages.${progname} = defaultPackage;
-          apps.${progname} = defaultApp;
-        }
-      );
+      drvBuilder = final: prev: (final.pkgs.callPackage ./Cargo.nix {}).rootCrate.build;
+    };
 }
